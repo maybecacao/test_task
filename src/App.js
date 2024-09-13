2f4b7e083ff7cc7b09028840b5ca52f0f32e56c1
maybecocoa <amojetkakao@gmail.com>
2024-09-13 12:07:30 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapPoints from './MapPoints';
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Map Points Application</h1>
+        <MapPoints />
+      </div>
+  );
+}
+
+export default App;
